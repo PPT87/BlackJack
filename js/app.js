20f1e75957d19b84c2f89d312fa029e1552f1ff2
PeterT <tranpp87@gmail.com>
2021-10-14 17:34:36 -0400

    update css, add buttons and init function

--- js/app.js
+++ js/app.js
@@ -3,19 +3,47 @@
 
 
 /*-------------------------------- Variables --------------------------------*/
-let player, dealer
+let playerHand, playerCount, dealerHand, dealerCount, deck, winner,turn, 
 
 
 /*------------------------ Cached Element References ------------------------*/
-const hitButton = document.querySelector("hitBtn")
-const standButton = document.querySelector("standBtn")
-const dealButton   = document.querySelector("dealBtn")
+const hitButton = document.querySelector("#hitBtn")
+const standButton = document.querySelector("#standBtn")
+const dealButton   = document.querySelector("#dealBtn")
+const homeButton = document.querySelector("#homeBtn")
 
 
 /*----------------------------- Event Listeners -----------------------------*/
+hitButton.addEventListener("click", () => {
+  console.log('Hit button is working')
+})
 
+standButton.addEventListener("click", () => {
+  console.log('Stand button is working')
+})
+
+dealButton.addEventListener("click", () => {
+  console.log('Deal button is working')
+})
+
+homeButton.addEventListener("click", () => {
+  console.log('Home button is working')
+})
 
 /*-------------------------------- Functions --------------------------------*/
-function test(){
-  console.log('hit button is working')
+function init(){
+  playerHand = [],
+  dealerHand = [],
+  playerCount = 0,
+  dealerCount = 0,
+  winner = false
+}
+
+
+function render(){
+
+}
+
+function getWinner(){
+
 }
