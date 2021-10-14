9cfc06f4fc723ed60c30af40074a93634383b3e0
PeterT <tranpp87@gmail.com>
2021-10-14 19:05:21 -0400

    add toggle switch and update css

--- js/app.js
+++ js/app.js
@@ -1,9 +1,20 @@
 /*-------------------------------- Constants --------------------------------*/
-
+const fullDeck = [
+  "dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02",
+  "hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02",
+  "cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02",
+  "sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"
+]
+// const fullDeck = [
+//   2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11,
+//   2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11,
+//   2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11,
+//   2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11,
+// ]
 
 
 /*-------------------------------- Variables --------------------------------*/
-let playerHand, playerCount, dealerHand, dealerCount, deck, winner,turn, 
+let playerHand, playerCount, dealerHand, dealerCount, deck, winner,turn, loser
 
 
 /*------------------------ Cached Element References ------------------------*/
@@ -36,7 +47,9 @@ function init(){
   dealerHand = [],
   playerCount = 0,
   dealerCount = 0,
-  winner = false
+  winner = false,
+  loser = false
+
 }
 
 
@@ -44,6 +57,17 @@ function render(){
 
 }
 
-function getWinner(){
+function isWinner(){
 
 }
+
+function isLoser(){
+
+}
+
+function shuffleDeck(fullDeck){
+  let randomCard = Math.floor(fullDeck.length * Math.random())
+  return fullDeck[randomCard]
+
+}
+console.log(shuffleDeck(fullDeck))
