0da6d6531453bfd6f4a6ab031870d96a49090636
PeterT <tranpp87@gmail.com>
2021-10-18 19:04:00 -0400

    add css sheet, add images, redo deck, compareHands()  working

--- js/app.js
+++ js/app.js
@@ -10,8 +10,6 @@ let dealerHand = []
 let playerCount = 0
 let dealerInitialCount = 0
 let dealerFinalCount = 0
-let dealerTurn = false
-let endRound = true
 let dealCard = 0
 let playerAce = 0
 let dealerAce = 0
@@ -56,9 +54,6 @@ console.log('light/dark button works')
 /*-------------------------------- Functions --------------------------------*/
 //When PLAY button is clicked, everything is reset and cards are dealt. 
 function gameStart(){
-  if (endRound === true){
-    dealerTurn = false
-    endRound = false
     messageEl.innerText = ""
     pTotalEl.innerText = ""
     dTotalEl.innerText = ""
@@ -66,7 +61,7 @@ function gameStart(){
     dealBtn.setAttribute("hidden", true)
     hitBtn.removeAttribute("hidden", true)
     standBtn.removeAttribute("hidden", true)
-  }
+  
   // Empty player and dealer hands
   if (playerHand.length > 0){
     playerHand.splice(0, playerHand.length)
@@ -79,11 +74,11 @@ function gameStart(){
   // Deal out initial cards to player
 function pInitialDeal(){
   for (let i=0; i<2; i++){ //loops 2 times to deal out 2 cards
-    dealCard = getRandomCard() // get 1 random card
-    playerHand.push(dealerDeck[dealCard]) // push random card to player hand. 
-    dealerDeck.splice(dealCard, 1)// remove random card from the deck
+    dealCard = getRandomCard() //get 1 random card
+    playerHand.push(dealerDeck[dealCard]) //pushes random card to dealer hand.
+    dealerDeck.splice(dealCard, 1) // remove random card from the deck
 }
-playerHandCount()
+  playerHandCount()
 }
 
 // Deal out initial cards to dealer
@@ -93,7 +88,6 @@ function dInitialDeal(){
     dealerHand.push(dealerDeck[dealCard]) //pushes random card to dealer hand.
     dealerDeck.splice(dealCard, 1) // remove random card from the deck
 }
-
 dealerHandCount()
 }
 
@@ -101,10 +95,10 @@ dealerHandCount()
 // get random card from dealerDeck
 function getRandomCard() {
   dealerDeck.push( 
-          "dA","dQ","dK","dJ","d10","d9","d8","d7","d6","d5","d4","d3","d2",
-          "hA","hQ","hK","hJ","h10","h9","h8","h7","h6","h5","h4","h3","h2",
-          "cA","cQ","cK","cJ","c10","c9","c8","c7","c6","c5","c4","c3","c2",
-          "sA","sQ","sK","sJ","s10","s9","s8","s7","s6","s5","s4","s3","s2")
+    "dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02",
+    "hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02",
+    "cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02",
+    "sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02")
   let randomCard = Math.floor(Math.random() * dealerDeck.length)
   return randomCard
 }
@@ -115,16 +109,16 @@ function playerHandCount(){
   playerAce = 0 // makes player aces count 0
   pHandVal.splice(0, pHandVal.length) //empty's player hand value
   for (i=0; i<playerHand.length; i++) { // so it can find the number of aces in hand
-    let value = playerHand[i].charAt(1); if (value === "A"){
+    let value = playerHand[i].slice(-1); if (value === "A"){ //looks at index 1 to see if the value is an "A"
       playerAce++ // adds how many aces you have
     } 
     else{ 
-      pHandVal.push(value) // converts the value into a number
+      pHandVal.push(value) 
     } 
   }
   for (i=0; i<(pHandVal.length); i++) { // loops through hand to see if you're holding any 10's or face cards
-    let value = pHandVal[i] 
-    if (value === "1" || value === "J" || value === "Q" || value === "K") {
+    let value = pHandVal[i] // saves the current card value to the varible 'value'
+    if (value === "0" || value === "J" || value === "Q" || value === "K") { // Assigns 10 to these values
         playerCount += 10 // add 10 to hand count
     } 
     else{ 
@@ -139,7 +133,6 @@ function playerHandCount(){
           }
       }
       if (playerCount > 21){
-        endRound = true
         playAgainBtn.removeAttribute("hidden", true)
         messageEl.innerText = `BUST! You Lose!`
         pTotalEl.innerText = `Total: ${playerCount}`
@@ -157,7 +150,7 @@ function playerHandCount(){
     dealerAce = 0
     dHandVal.splice(0, dHandVal.length)
     for (i=0; i<dealerHand.length; i++) {
-      let value = dealerHand[i].charAt(1); if (value === "A"){
+      let value = dealerHand[i].slice(-1); if (value === "A"){
         dealerAce++
       } 
       else{ 
@@ -167,7 +160,7 @@ function playerHandCount(){
     
     for (i=0; i<(dHandVal.length); i++) { 
       let value = dHandVal[i] 
-      if (value === "1" || value === "J" || value === "Q" || value === "K") {
+      if (value === "0" || value === "J" || value === "Q" || value === "K") {
           dealerFinalCount += 10 
       } 
       else { 
@@ -182,7 +175,6 @@ function playerHandCount(){
         }
       }
         if (dealerFinalCount > 21){
-        endRound = true
         playAgainBtn.removeAttribute("hidden", true)
         messageEl.innerText = `Dealer BUST! You Win!`
         dTotalEl.innerText = `Total: ${dealerFinalCount}`
@@ -195,18 +187,15 @@ function playerHandCount(){
 
 // Pushes randomCard into players hand and removes card of dealerDeck
 function hitMe(){
-  if (endRound === false){
-    randomCard = getRandomCard()
-    playerHand.push(dealerDeck[randomCard])
-    dealerDeck.splice(randomCard, 1)
-  } 
+  randomCard = getRandomCard()
+  playerHand.push(dealerDeck[randomCard])
+  dealerDeck.splice(randomCard, 1)
   playerHandCount()
 }
 
+
 // If dealer is < player, draw card
 function stand(){
-  if (endRound === false){
-    dealerTurn = true
     if (dealerFinalCount < playerCount){
       randomCard = getRandomCard()
       dealerHand.push(dealerDeck[randomCard])
@@ -215,36 +204,31 @@ function stand(){
     else{
       endRound = true
     }
+    dealerHandCount()
+    compareHands()
+    playAgainBtn.removeAttribute("hidden", true)
   }
-  dealerHandCount()
-  compareHands()
-  playAgainBtn.removeAttribute("hidden", true)
-}
 
 // Determines if you're a winner, loser or tie game. 
 function compareHands(){
-  if (endRound === true){
-    if (playerHandCount > dealerFinalCount){
-      messageEl.innerText = `You Win!`
-      endRound = true
+    console.log(playerCount, dealerFinalCount)
+    if (playerCount > dealerFinalCount){
+      messageEl.innerHTML = `You Win!`
       playAgainBtn.remove("hidden", true)
       return
     } 
-    else if (playerHandCount < dealerFinalCount){
-      messageEl.innerText = `You Lose!`
-      endRound = true
+    else if (playerCount < dealerFinalCount){
+      messageEl.innerHTML = `You Lose!`
       playAgainBtn.remove("hidden", true)
       return
     }
-    else if (playerHandCount === dealerFinalCount){
-      messageEl.innerText = `It's a Tie!`
-      endRound = true
+    else if (playerCount === dealerFinalCount){
+      messageEl.innerHTML = `It's a Tie!`
       playAgainBtn.remove("hidden", true)
       return
     }
     
   }
-}
 
 function home(){
   window.location.reload()
