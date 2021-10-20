908f95a6c2ef7acf928e9c1bbcffb86120d33735
PeterT <tranpp87@gmail.com>
2021-10-20 15:27:10 -0400

    redo code and add some styling

--- js/app.js
+++ js/app.js
@@ -1,21 +1,23 @@
 /*-------------------------------- Constants --------------------------------*/
 const switchTheme = document.querySelector("#switch");
 
-
+const dealerDeck =[  
+  "dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02",
+  "hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02",
+  "cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02",
+  "sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"]
 
 /*-------------------------------- Variables --------------------------------*/
 // Declare deck variables
-let dealerDeck = []
 let playerHand = []
 let dealerHand = []
-let playerCount = 0
-let dealerInitialCount = 0
-let dealerFinalCount = 0
-let dealCard = 0
-let playerAce = 0
-let dealerAce = 0
+let playerSum = 0
+let dealerSum = 0
+let playerAces = 0
+let dealerAces = 0
 let pHandVal = [] 
 let dHandVal = [] 
+
 /*------------------------ Cached Element References ------------------------*/
 // Cached element references
 let lightDarkBtn = document.getElementById('lightDarkBtn')
@@ -26,223 +28,188 @@ let hitBtn = document.getElementById('hitBtn')
 let standBtn = document.getElementById('standBtn')
 let messageEl = document.getElementById('message')
 let playerCardsEl = document.getElementById('playerCards')
-let pCard1El = document.getElementById('pCard1El')
-let pCard2El = document.getElementById('pCard2El')
 let pCardEl = document.getElementById('pCardEl')
-let dCard1El = document.getElementById('dCard1El')
-let dCard2El = document.getElementById('dCard2El')
 let dCardEl = document.getElementById('dCardEl')
 let pTotalEl = document.getElementById('pTotal')
 let dTotalEl = document.getElementById('dTotal')
 
-
-
-
 /*----------------------------- Event Listeners -----------------------------*/
 // Event listeners
 document.getElementById('homeBtn').addEventListener('click', home)
-document.getElementById('dealBtn').addEventListener('click', gameStart)
-document.getElementById('playAgainBtn').addEventListener('click', gameStart)
+document.getElementById('dealBtn').addEventListener('click', init)
+document.getElementById('playAgainBtn').addEventListener('click', init)
 document.getElementById('hitBtn').addEventListener('click', hitMe)
 document.getElementById('standBtn').addEventListener('click', stand)
 document.getElementById('lightDarkBtn').addEventListener('click', lightDark)
 
 /*-------------------------------- Functions --------------------------------*/
-//When PLAY button is clicked, everything is reset and cards are dealt. 
+function init(){
+  messageEl.innerText = ""
+  pTotalEl.innerText = ""
+  dTotalEl.innerText = ""
+  let playerHand = []
+  let dealerHand = []
+  let playerSum = 0
+  let dealerSum = 0
+  let playerAces = 0
+  let dealerAces = 0
+  playAgainBtn.setAttribute("hidden", true)
+  dealBtn.setAttribute("hidden", true)  
+  hitBtn.removeAttribute("hidden", true)
+  standBtn.removeAttribute("hidden", true)
+  gameStart()
+}
+
 function gameStart(){
-    messageEl.innerText = ""
-    pTotalEl.innerText = ""
-    dTotalEl.innerText = ""
-    playAgainBtn.setAttribute("hidden", true)
-    dealBtn.setAttribute("hidden", true)
-    hitBtn.removeAttribute("hidden", true)
-    standBtn.removeAttribute("hidden", true)
-    pCard1El.classList.remove(playerHand[0])
-    pCard2El.classList.remove(playerHand[1])
-    dCard1El.classList.remove(dealerHand[0])
-  
-  // Empty player and dealer hands
-  if (playerHand.length > 0){
-    playerHand.splice(0, playerHand.length)
-    dealerHand.splice(0, dealerHand.length)
-  } 
-  pInitialDeal()
-  dInitialDeal()
-  }
-
-  function pInitialDeal(){
-    for (let i=0; i<2; i++){ //loops 2 times to deal out 2 cards
-      dealCard = getRandomCard() //get 1 random card
-      playerHand.push(dealerDeck[dealCard]) //pushes random card to dealer hand.
-      dealerDeck.splice(dealCard, 1) // remove random card from the deck
-  }
-    playerHandCount()
-    pCard1El.classList.add(playerHand[0])
-    pCard2El.classList.add(playerHand[1])
-  }
-
-// Deal out initial cards to dealer
-function dInitialDeal(){
-  for (let i=0; i<2; i++){ //loops 2 times to deal out 2 cards
-    dealCard = getRandomCard() //get 1 random card
-    dealerHand.push(dealerDeck[dealCard]) //pushes random card to dealer hand.
-    dealerDeck.splice(dealCard, 1) // remove random card from the deck
-}
-dealerInitialSum()
-dCard1El.classList.add(dealerHand[0])
+  hitMe()
+  hitMe()
+  dealerHit()
+  dealerHit()
 }
 
-//  Get dealer initial hand
-function dealerInitialSum(){
-  dealerInitialCount=0
-  value = dealerHand[0].slice(-1)
-  if (value === "A") { //If the card is an ace show value of 11
-  dealerInitialCount += 11
-  // If the card is valued at 1 (10), J, Q, K add 10 to the dealer sum
-  } else if (value === "0" || value === "J" || value === "Q" || value === "K") {
-      dealerInitialCount += 10
-  } else { // Otherwise, add the face value of the card to the dealer sum.
-      dealerInitialCount += parseInt(value) //parseInt converts the number to an integer
-  }
-  dTotalEl.innerText = `Total: ${dealerInitialCount}`
-}
-
-// get random card from dealerDeck
-function getRandomCard() {
-  dealerDeck.push( 
-    "dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02",
-    "hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02",
-    "cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02",
-    "sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02")
-  let randomCard = Math.floor(Math.random() * dealerDeck.length)
+function pRandomCard(){// Flippin' awesome code. This grabs a random card from the deck
+  let randIdx = Math.floor(Math.random() * dealerDeck.length)
+  let randomCard = dealerDeck.splice(randIdx, 1)
+  playerHand.push(randomCard)
+  playerSum += getPlayerSum()
+  pTotalEl.innerHTML = `Total: ${playerSum}`
+  console.log(`player`, playerHand, playerSum)
   return randomCard
 }
 
-// Get total of player hand
-function playerHandCount(){
-  playerCount = 0 // makes player count 0
-  playerAce = 0 // makes player aces count 0
-  pHandVal.splice(0, pHandVal.length) //empty's player hand value
-  for (i=0; i<playerHand.length; i++) { // so it can find the number of aces in hand
-    let value = playerHand[i].slice(-1); if (value === "A"){ //looks at index 1 to see if the value is an "A"
-      playerAce++ // adds how many aces you have
-    } 
-    else{ 
-      pHandVal.push(value) 
-    } 
-  }
-  for (i=0; i<(pHandVal.length); i++) { // loops through hand to see if you're holding any 10's or face cards
-    let value = pHandVal[i] // saves the current card value to the variable 'value'
-    if (value === "0" || value === "J" || value === "Q" || value === "K") { // Assigns 10 to these values
-        playerCount += 10 // add 10 to hand count
-    } 
-    else{ 
-        playerCount += parseInt(value) // converts the other values to a number
-      } 
-      if (playerAce > 0) { 
-        if (playerCount + 11 > 21){ // Checks to see if the ace will make total go over 21
-            playerCount += 1 //if it does, make the ace to a 1
-          }
-          else{ // if the ace being 11 doesn't got over 21, make the ace a value of 11
-            playerCount += 11
-          }
-      }
-      if (playerCount > 21){
-        playAgainBtn.removeAttribute("hidden", true)
-        messageEl.innerText = `BUST! You Lose!`
-        pTotalEl.innerText = `Total: ${playerCount}`
-        return
-      }
-    }
-    console.log(`player`, playerHand, playerCount)
-    pTotalEl.innerText = `Total: ${playerCount}` 
-  }
+// Deal out initial cards to dealer
+function dRandomCard(){// Flippin' awesome code. This grabs a random card from the deck
+  let randIdx = Math.floor(Math.random() * dealerDeck.length)
+  let randomCard = dealerDeck.splice(randIdx, 1)
+  dealerHand.push(randomCard)
+  dealerSum += getDealerSum()
+  dTotalEl.innerHTML = `Total: ${dealerSum}`
+  console.log(`dealer`, dealerHand, dealerSum)
+  return randomCard
+} 
 
-  //  Get total of dealer hand
-  function dealerHandCount(){
-    dealerFinalCount = 0
-    dealerAce = 0
-    dHandVal.splice(0, dHandVal.length)
-    for (i=0; i<dealerHand.length; i++) {
-      let value = dealerHand[i].slice(-1); if (value === "A"){
-        dealerAce++
-      } 
-      else{ 
-        dHandVal.push(value) 
-      } 
-    }
-    
-    for (i=0; i<(dHandVal.length); i++) { 
-      let value = dHandVal[i] 
-      if (value === "0" || value === "J" || value === "Q" || value === "K") {
-          dealerFinalCount += 10 
-      } 
-      else { 
-          dealerFinalCount += parseInt(value) 
-      }
-      if (dealerAce > 0) { 
-        if (dealerFinalCount + 11 > 21){ // Check to see if one of the aces (more than 1) being 11 will put the total over 21
-            dealerFinalCount += 1 //if it does, only add the total number of aces to the total
-        }
-        else{ // if the ace being 11 doesn't got over 21, add 11 plus the number of aces minus 1 (the one you made 11)
-          dealerFinalCount += 11 
-        }
-      }
-    }
-    console.log(`dealer`, dealerHand, dealerFinalCount)
-    dTotalEl.innerText = `Total: ${dealerFinalCount}`
+function getPlayerSum(){
+  let value = playerHand[playerHand.length - 1].toString()
+  let endValue = value.slice(-1)
+  if (endValue === "0" || endValue === "J" || endValue === "Q" || endValue === "K"){
+    return 10
   }
-
-// Pushes randomCard into players hand and removes card of dealerDeck
-function hitMe(){
-  randomCard = getRandomCard()
-  playerHand.push(dealerDeck[randomCard])
-  dealerDeck.splice(randomCard, 1)
-  playerHandCount()
-  console.log(`playerhit`, playerHand)
+  else if(endValue === "A"){
+    return checkPlayerAces()
+  }
+  else{
+    return parseInt(endValue)
+  }
 }
 
-// Dealer turn to make it's move
-function stand(){
-  while (dealerFinalCount < 17){ // while the dealer is <17 it will continue to draw a card
-      if(dealerFinalCount >= playerCount){ // if the dealer count >= the player count. break out of the loop
-        break 
-      }
-      else{ // computer will draw until it meets the if statement
-        randomCard = getRandomCard()
-        dealerHand.push(dealerDeck[randomCard])
-        dealerDeck.splice(randomCard, 1)
-        dealerHandCount()
-        console.log(dealerFinalCount)
-        console.log(`dealerstand`, dealerHand, dealerFinalCount)
-      }  
+function getDealerSum(){
+  let value = dealerHand[dealerHand.length - 1].toString()
+  let endValue = value.slice(-1)
+  if (endValue === "0" || endValue === "J" || endValue === "Q" || endValue === "K"){
+    return 10
   }
-    compareHands()
+  else if(endValue === "A"){
+    return checkDealerAces()
+  }
+  else{
+    return parseInt(endValue)
+  }
+}
+
+function checkPlayerAces(){
+  if(playerSum + 11 > 21){
+    return 1
+  }
+  else{
+    // playerAces = 1
+    return 11
+  }
+}
+
+function checkDealerAces(){
+  if(dealerSum + 11 > 21){
+    return 1
+  }
+  else{
+    dealerAces = 1
+    return 11
+  }
+}
+
+// Draws 1 random card and recalculates total. If over 21, you lose
+function hitMe(){
+  const newDiv = document.createElement('div');
+  newDiv.className = "card large"
+  pCardEl.appendChild(newDiv)
+  newDiv.classList.add(pRandomCard())
+  if(playerSum > 21){
+    messageEl.innerHTML = `BUST! You Lose!`
     playAgainBtn.removeAttribute("hidden", true)
-    dCard2El.classList.add(dealerHand[1])
+    hitBtn.setAttribute("hidden", true)
+    standBtn.setAttribute("hidden", true)
+    getPlayerSum()
+    return
   }
+}
 
-// Determines if you're a winner, loser or tie game. 
-function compareHands(){
+// Daws 1 random card for dealer
+function dealerHit(){
+  const newDiv = document.createElement('div');
+  newDiv.className = "card large"
+  dCardEl.appendChild(newDiv)
+  newDiv.classList.add(dRandomCard())
+  console.log(`newdiv`)
+}
 
-  if (dealerFinalCount > 21){
+// Computer will run and check conditions below
+function stand(){
+  while (dealerSum <= 17 ){ // while the dealer is <17 it will continue to draw a card
+    const newDiv = document.createElement('div');
+    newDiv.className = "card large"
+    dCardEl.appendChild(newDiv)
+    newDiv.classList.add(dRandomCard())
+    isWinner()
+    if(dealerSum >= playerSum || dealerSum > 21){ // if the dealer count >= the player count. break out of the loop
+      isWinner()
+      playAgainBtn.removeAttribute("hidden", true)
+      hitBtn.setAttribute("hidden", true)
+      standBtn.setAttribute("hidden", true)
+      break
+    } 
+  }
+}
+
+// Check to see win/lose conditions
+function isWinner(){
+  if (dealerSum > 21){
     messageEl.innerHTML = `Dealer BUSTS! You Win!`
   }
-  else if(playerCount > dealerFinalCount) {
+  else if (playerSum === 21 && dealerSum < playerSum){
+    messageEl.innerHTML = ` BLACKJACK! You Win!`
+  }
+  else if (dealerSum === 21 && dealerSum > playerSum){
+      messageEl.innerHTML = `Dealer BLACKJACK! You Lose!`
+  }
+  else if(playerSum > dealerSum) {
       messageEl.innerHTML = `You Win!`
   } 
-  else if(playerCount < dealerFinalCount || playerCount > 21){
+  else if(playerSum < dealerSum){
       messageEl.innerHTML = `You Lose!`
   }
   else{
       messageEl.innerHTML = `It's a Tie!`
   }
-  }
+  playAgainBtn.removeAttribute("hidden", true)
+  hitBtn.setAttribute("hidden", true)
+  standBtn.setAttribute("hidden", true)
+}
 
+// Refresh the page to main screen
 function home(){
   window.location.reload()
 }
 
+// Toggles light/dark mode
 function lightDark() {
   let element = document.body;
   element.classList.toggle("dark-mode");
