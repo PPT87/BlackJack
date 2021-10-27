f820cc9f852af0d1164bc065022523b99f5a4aec
PeterT <tranpp87@gmail.com>
2021-10-26 22:51:37 -0400

    move deck to variable

--- js/app.js
+++ js/app.js
@@ -1,13 +1,6 @@
 /*-------------------------------- Constants --------------------------------*/
 const switchTheme = document.querySelector("#switch");
 
-let dealerDeck = [
-  "dA", "dQ", "dK", "dJ", "d10", "d09", "d08", "d07", "d06", "d05", "d04", "d03", "d02",
-  "hA", "hQ", "hK", "hJ", "h10", "h09", "h08", "h07", "h06", "h05", "h04", "h03", "h02",
-  "cA", "cQ", "cK", "cJ", "c10", "c09", "c08", "c07", "c06", "c05", "c04", "c03", "c02",
-  "sA", "sQ", "sK", "sJ", "s10", "s09", "s08", "s07", "s06", "s05", "s04", "s03", "s02"
-]
-
 /*-------------------------------- Variables --------------------------------*/
 let playerHand = []
 let dealerHand = []
@@ -15,6 +8,12 @@ let pHandVal = []
 let dHandVal = []
 let playerSum = 0
 let dealerSum = 0
+let dealerDeck = [
+  "dA", "dQ", "dK", "dJ", "d10", "d09", "d08", "d07", "d06", "d05", "d04", "d03", "d02",
+  "hA", "hQ", "hK", "hJ", "h10", "h09", "h08", "h07", "h06", "h05", "h04", "h03", "h02",
+  "cA", "cQ", "cK", "cJ", "c10", "c09", "c08", "c07", "c06", "c05", "c04", "c03", "c02",
+  "sA", "sQ", "sK", "sJ", "s10", "s09", "s08", "s07", "s06", "s05", "s04", "s03", "s02"
+]
 
 /*------------------------ Cached Element References ------------------------*/
 let lightDarkBtn = document.getElementById('lightDarkBtn')
